41c9cf4e724b6d1e4e9c9927ce52b6fe5a65af2c
Scover <bardini.raphael@gmail.com>
2025-02-21 17:30:53 +0100

    feat: add sapphire ore processing recipes

--- src/gtceu.py
+++ src/gtceu.py
@@ -7,14 +7,26 @@ from valuedset import ValuedSet
 
 
 class Oc(Enum):
+    """Energy tiers"""
     ULV = 0
-    LV = 1
-    MV = 2
-    HV = 3
-    EV = 4
+    LV = 0
+    MV = 1
+    HV = 2
+    EV = 3
+    IV = 4
+    LuV = 5
+    ZPM = 6
+    UV = 7
+    UHV = 8
+    UEV = 9
+    UIV = 10
+    UXV = 11
+    OpV = 12
+    MAX = 13
 
 
 TIER = Oc.LV
+# todo: tier chance bonues
 
 
 class Type(Enum):
@@ -25,13 +37,21 @@ class Type(Enum):
 
 
 class Machine(Enum):
-    MACERATOR = 'Macerator'
-    ORE_WASHER = 'Ore Washer'
-    CHEMICAL_BATH = 'Chemical Bath'
-    THERMAL_CENTRIFUGE = 'Thermal Centrifuge'
     CENTRIFUGE = 'Centrifuge'
+    PACKER = 'Packer'
+    CUTTER = 'Cutter'
+    ELECTROLYZER = 'Electrolyzer'
+    LATHE = 'Lathe'
+    MACERATOR = 'Macerator'
+    ORE_CRUSHING = 'Ore Crushing (~Forge Hammer)'
+    ORE_GRINDING = 'Ore Grinding (~Macerator)'
+    ORE_TREATING = 'Ore Treating (~Chemical Bath)'
+    ORE_WASHER = 'Ore Washer'
+    PART_GRINDING = 'Part Grinding'
+    SIFTER = 'Sifter'
     SMELTING = 'Smelting'
-    FORGE_HAMMER = 'Forge Hammer'
+    THERMAL_CENTRIFUGE = 'Thermal Centrifuge'
+    IMPLOSION_COMPRESSOR = 'Implosion Compressor'
 
 
 @dataclass(frozen=True)
@@ -46,6 +66,7 @@ class Resource:
 
 @dataclass(frozen=True)
 class Recipe:
+    id: int
     machine: Machine
     inputs: ValuedSet[Resource]
     outputs: ValuedSet[Resource]
@@ -53,31 +74,53 @@ class Recipe:
     'seconds'
     energy: float
     'eu'
+    circuit_config_no: int | None
     times: float
 
-    def __mul__(self, other: float):
-        return Recipe(self.machine, self.inputs * other, self.outputs * other, self.duration * other, self.energy * other, other)
+    def __mul__(self, times: float):
+        return Recipe(
+            self.id,
+            self.machine,
+            self.inputs * times,
+            self.outputs * times,
+            self.duration * times,
+            self.energy * times,
+            self.circuit_config_no,
+            times,
+        )
 
     def to_lines(self, index: int) -> Sequence[Line]:
         def flow_lines(flows: ValuedSet[Resource]):
             return (Line.fixed(f'{round(quantity, 3):g} {resource}') for resource, quantity in flows.items())
 
-        return (Line.space_between(f'Recipe {index} ({self.times:g}x)', self.machine.value),
+        return (Line.space_between(f'{index}. {self.times:g}x Recipe {self.id}', self.machine.value + ('' if self.circuit_config_no is None else f', config {self.circuit_config_no}')),
                 *flow_lines(self.inputs),
                 Line.fixed('\u21a7'),
                 *flow_lines(self.outputs),
                 Line.space_between(f'{self.energy:g}EU', f'{self.duration:g}s'))
 
 
+_recipe_id = 0
+
+
 def recipe(machine: Machine, inputs: Mapping[Resource, float],
            outputs: Mapping[Resource, float],
-           duration: float, energy: float, start_oc: Oc | None = None):
-    'start_oc: the OC after which efficiency increases'
-    if start_oc is not None:
-        mult = 2 ** (TIER.value - start_oc.value)
-        energy *= mult
-        duration /= mult
-    return Recipe(machine, ValuedSet(inputs), ValuedSet(outputs), duration, energy, 1)
+           duration: float, energy: float, circuit_config_no: int | None = None):
+    if machine is not Machine.SMELTING:
+        mult = 2 ** TIER.value
+        energy = min(131072, energy * mult)
+        duration = max(.05, duration / mult)
+    global _recipe_id
+    return Recipe(
+        _recipe_id := _recipe_id + 1,
+        machine,
+        ValuedSet(inputs),
+        ValuedSet(outputs),
+        duration,
+        energy,
+        circuit_config_no,
+        1,
+    )
 
 
 @dataclass(frozen=True)
@@ -88,12 +131,12 @@ class Chain:
     @property
     @cache
     def cost(self):
-        return sum(res.value * -qty for res, qty in self.flows.items() if qty < 0)
+        return sum(rs.value * -qty for rs, qty in self.flows.items() if qty < 0)
 
     @property
     @cache
     def value(self):
-        return sum(res.value * qty for res, qty in self.flows.items() if qty > 0)
+        return sum(rs.value * qty for rs, qty in self.flows.items() if qty > 0)
 
     @property
     @cache
@@ -104,6 +147,14 @@ class Chain:
     @cache
     def duration(self):
         return sum(r.duration for r in self.recipes)
+    
+    @property
+    @cache
+    def id(self):
+        return ','.join(str(r.id) for r in self.recipes)
+    
+    def __len__(self) -> int:
+        return len(self.recipes)
 
 
 def chain(recipes: Iterable[Recipe]):
--- src/recipestats.py
+++ src/recipestats.py
@@ -1,23 +1,46 @@
 #!/bin/env python3
 
-from gtceu import chain, Chain, Resource, Type, Recipe, recipe, Machine, Oc, TIER
+from gtceu import chain, Chain, Resource, Type, Recipe, recipe, Machine, TIER
 from lines import print_box
 
 R = {
     'Cobalt Dust': Resource(Type.ITEM, 'Cobalt Dust', 100),
     'Sulfur Dust': Resource(Type.ITEM, 'Sulfur Dust', 25),
     'Stone Dust': Resource(Type.ITEM, 'Stone Dust', 5),
-    'Sodium Persulfate': Resource(Type.FLUID, 'Sodium Persulfate', .67),
-    'Water': Resource(Type.FLUID, 'Water', .002),
-    'Distilled Water': Resource(Type.FLUID, 'Distilled Water', .025),
-    'Cobalt Ingot': Resource(Type.ITEM, 'Cobalt Ingot', 0),
-    'Cobaltite Dust': Resource(Type.ITEM, 'Cobaltite Dust', 0),
-    'Crushed Cobaltite Ore': Resource(Type.ITEM, 'Crushed Cobaltite Ore', 0),
-    'Impure Pile of Cobaltite Dust': Resource(Type.ITEM, 'Impure Pile of Cobaltite Dust', 0),
-    'Purified Cobaltite Ore': Resource(Type.ITEM, 'Purified Cobaltite Ore', 0),
-    'Purified Pile of Cobaltite Dust': Resource(Type.ITEM, 'Purified Pile of Cobaltite Dust', 0),
-    'Raw Cobaltite': Resource(Type.ITEM, 'Raw Cobaltite', 0),
-    'Refined Cobaltite Ore': Resource(Type.ITEM, 'Refined Cobaltite Ore', 0),
+    'Sodium Persulfate': Resource(Type.FLUID, 'Sodium Persulfate', .9),
+    'Water': Resource(Type.FLUID, 'Water', 0),
+    'Distilled Water': Resource(Type.FLUID, 'Distilled Water', .001),
+    'Cobalt Ingot': Resource(Type.ITEM, 'Cobalt Ingot', 1),
+    'Cobaltite Dust': Resource(Type.ITEM, 'Cobaltite Dust', 1),
+    'Crushed Cobaltite Ore': Resource(Type.ITEM, 'Crushed Cobaltite Ore', 1),
+    'Impure Pile of Cobaltite Dust': Resource(Type.ITEM, 'Impure Pile of Cobaltite Dust', 1),
+    'Purified Cobaltite Ore': Resource(Type.ITEM, 'Purified Cobaltite Ore', 1),
+    'Purified Pile of Cobaltite Dust': Resource(Type.ITEM, 'Purified Pile of Cobaltite Dust', 1),
+    'Raw Cobaltite': Resource(Type.ITEM, 'Raw Cobaltite', 1),
+    'Refined Cobaltite Ore': Resource(Type.ITEM, 'Refined Cobaltite Ore', 1),
+
+    'Raw Sapphire': Resource(Type.ITEM, 'Raw Sapphire', 1),
+    'Sapphire': Resource(Type.ITEM, 'Sapphire', 1),
+    'Crushed Sapphire Ore': Resource(Type.ITEM, 'Crushed Sapphire Ore', 1),
+    'Aluminium Dust': Resource(Type.ITEM, 'Aluminium Dust', 100),
+    'Purified Sapphire Ore': Resource(Type.ITEM, 'Purified Sapphire Ore', 1),
+    'Exquisite Sapphire': Resource(Type.ITEM, 'Exquisite Sapphire', 1),
+    'Flawless Sapphire': Resource(Type.ITEM, 'Flawless Sapphire', 1),
+    'Purified Pile of Sapphire Dust': Resource(Type.ITEM, 'Purified Pile of Sapphire Dust', 1),
+    'Impure Pile of Sapphire Dust': Resource(Type.ITEM, 'Impure Pile of Sapphire Dust', 1),
+    'Green Sapphire Dust': Resource(Type.ITEM, 'Green Sapphire Dust', 1),
+    'Sapphire Dust': Resource(Type.ITEM, 'Sapphire Dust', 1),
+    'Small Pile of Sapphire Dust': Resource(Type.ITEM, 'Small Pile of Sapphire Dust', 1),
+    'Refined Sapphire Ore': Resource(Type.ITEM, 'Refined Sapphire Ore', 1),
+    'Oxygen Gas': Resource(Type.FLUID, 'Oxygen Gas', 1),
+    'Lubricant': Resource(Type.FLUID, 'Lubricant', 1),
+    'Sapphire Lens': Resource(Type.ITEM, 'Sapphire Lens', 1),
+
+    # 'Dynamite': Resource(Type.ITEM, 'Dynamite', 1),
+    # 'Paper': Resource(Type.ITEM, 'Paper', 1),
+    # 'Glyceryl Trinitrate': Resource(Type.FLUID, 'Glyceryl Trinitrate', 1),
+    # 'TNT': Resource(Type.ITEM, 'TNT', 1),
+    # 'Powderbarrel': Resource(Type.ITEM, 'Powderbarrel', 1),
 }
 
 SMELTING_SECS = 10
@@ -31,99 +54,101 @@ def chance(probability: float, boost: float) -> float:
 
 
 RECIPES = (
+    # Cobaltite processing
+
     recipe(Machine.SMELTING,
            {R['Raw Cobaltite']: 1},
            {R['Cobalt Ingot']: 1},
            SMELTING_SECS, SMELTING_EU),
-    recipe(Machine.FORGE_HAMMER,
+    recipe(Machine.ORE_CRUSHING,
            {R['Raw Cobaltite']: 1},
            {R['Crushed Cobaltite Ore']: 1},
-           .5, 160, Oc.LV),
-    recipe(Machine.MACERATOR,
+           .5, 160),
+    recipe(Machine.ORE_GRINDING,
            {R['Raw Cobaltite']: 1},
            {R['Crushed Cobaltite Ore']: 2,
             R['Sulfur Dust']: chance(.1, .03),
             R['Stone Dust']: chance(.5, .01)},
-           20, 800, Oc.LV),
+           20, 800),
 
     recipe(Machine.SMELTING,
            {R['Crushed Cobaltite Ore']: 1},
            {R['Cobalt Ingot']: 1},
            SMELTING_SECS, SMELTING_EU),
-    #!recipe(Machine.CHEMICAL_BATH,
+    #!recipe(Machine.ORE_TREATING,
     #!       {R['Crushed Cobaltite Ore']: 1,
     #!        R['Sodium Persulfate']: 100},
     #!       {R['Purified Cobaltite Ore']: 1,
     #!        R['Cobalt Dust']: chance(.7, .058),
     #!        R['Stone Dust']: chance(.4, .065)},
-    #!       10, 6000, Oc.LV),
-    recipe(Machine.FORGE_HAMMER,
+    #!       10, 6000),
+    recipe(Machine.ORE_CRUSHING,
            {R['Crushed Cobaltite Ore']: 1},
            {R['Impure Pile of Cobaltite Dust']: 1},
-           .5, 160, Oc.LV),
-    recipe(Machine.MACERATOR,
+           .5, 160),
+    recipe(Machine.ORE_GRINDING,
            {R['Crushed Cobaltite Ore']: 1},
            {R['Impure Pile of Cobaltite Dust']: 1,
             R['Sulfur Dust']: chance(.14, .085)},
-           20, 800, Oc.LV),
+           20, 800),
     recipe(Machine.ORE_WASHER,
            {R['Crushed Cobaltite Ore']: 1,
             R['Water']: 1000},
            {R['Purified Cobaltite Ore']: 1,
             R['Sulfur Dust']: 1/3,
             R['Stone Dust']: 1},
-           20, 6400, Oc.LV),
+           20, 6400, 1),
     recipe(Machine.ORE_WASHER,
            {R['Crushed Cobaltite Ore']: 1,
             R['Distilled Water']: 100},
            {R['Purified Cobaltite Ore']: 1,
             R['Sulfur Dust']: 1/3,
             R['Stone Dust']: 1},
-           10, 3200, Oc.LV),
+           10, 3200),
     recipe(Machine.ORE_WASHER,
            {R['Crushed Cobaltite Ore']: 1,
             R['Water']: 100},
            {R['Purified Cobaltite Ore']: 1},
-           .4, 32, Oc.LV),
+           .4, 32, 2),
     recipe(Machine.THERMAL_CENTRIFUGE,
            {R['Crushed Cobaltite Ore']: 1},
            {R['Refined Cobaltite Ore']: 1,
             R['Cobalt Dust']: 1/3,
             R['Stone Dust']: 1},
-           20, 12000, Oc.LV),
+           20, 12000),
 
     recipe(Machine.SMELTING,
            {R['Purified Cobaltite Ore']: 1},
            {R['Cobalt Ingot']: 1},
            SMELTING_SECS, SMELTING_EU),
-    recipe(Machine.FORGE_HAMMER,
+    recipe(Machine.ORE_CRUSHING,
            {R['Purified Cobaltite Ore']: 1},
            {R['Purified Pile of Cobaltite Dust']: 1},
-           .5, 160, Oc.LV),
-    recipe(Machine.MACERATOR,
+           .5, 160),
+    recipe(Machine.ORE_GRINDING,
            {R['Purified Cobaltite Ore']: 1},
            {R['Purified Pile of Cobaltite Dust']: 1,
             R['Cobalt Dust']: chance(.14, .085)},
-           20, 800, Oc.LV),
+           20, 800),
     recipe(Machine.THERMAL_CENTRIFUGE,
            {R['Purified Cobaltite Ore']: 1},
            {R['Refined Cobaltite Ore']: 1,
             R['Cobalt Dust']: 1/3},
-           20, 12000, Oc.LV),
+           20, 12000),
 
     recipe(Machine.SMELTING,
            {R['Refined Cobaltite Ore']: 1},
            {R['Cobalt Ingot']: 1},
            SMELTING_SECS, SMELTING_EU),
-    recipe(Machine.FORGE_HAMMER,
+    recipe(Machine.ORE_CRUSHING,
            {R['Refined Cobaltite Ore']: 1},
            {R['Cobaltite Dust']: 1},
-           .5, 160, Oc.LV),
-    recipe(Machine.MACERATOR,
+           .5, 160),
+    recipe(Machine.ORE_GRINDING,
            {R['Refined Cobaltite Ore']: 1},
            {R['Cobaltite Dust']: 1,
             R['Cobalt Dust']: chance(.14, .085)},
-           20, 800, Oc.LV),
+           20, 800),
 
     recipe(Machine.SMELTING,
            {R['Impure Pile of Cobaltite Dust']: 1},
@@ -133,7 +158,7 @@ RECIPES = (
            {R['Impure Pile of Cobaltite Dust']: 1},
            {R['Cobaltite Dust']: 1,
             R['Sulfur Dust']: 1/9},
-           11, 5280, Oc.LV),
+           11, 5280),
     recipe(Machine.ORE_WASHER,
            {R['Impure Pile of Cobaltite Dust']: 1,
             R['Water']: 100},
@@ -144,7 +169,7 @@ RECIPES = (
            {R['Purified Pile of Cobaltite Dust']: 1},
            {R['Cobaltite Dust']: 1,
             R['Cobalt Dust']: 1/9},
-           5, 500, Oc.LV),
+           5, 500),
     recipe(Machine.ORE_WASHER,
            {R['Purified Pile of Cobaltite Dust']: 1,
             R['Water']: 100},
@@ -159,7 +184,125 @@ RECIPES = (
     recipe(Machine.MACERATOR,
            {R['Cobalt Ingot']: 1},
            {R['Cobalt Dust']: 1},
-           2.95, 118, Oc.LV)
+           2.95, 118),
+
+    # Sapphire ore processing
+
+    recipe(Machine.SMELTING,
+           {R['Raw Sapphire']: 1},
+           {R['Sapphire']: 1},
+           SMELTING_SECS, SMELTING_EU),
+    recipe(Machine.ORE_CRUSHING,
+           {R['Raw Sapphire']: 1},
+           {R['Sapphire']: 1},
+           .5, 160),
+    recipe(Machine.ORE_GRINDING,
+           {R['Raw Sapphire']: 1},
+           {R['Crushed Sapphire Ore']: 2, R['Aluminium Dust']: .1, R['Stone Dust']: .05},
+           20, 800),
+    recipe(Machine.ORE_WASHER,
+           {R['Crushed Sapphire Ore']: 1, R['Water']: 1000},
+           {R['Purified Sapphire Ore']: 1, R['Aluminium Dust']: 1/3, R['Stone Dust']: 1},
+           20, 6400, 1),
+    recipe(Machine.ORE_WASHER,
+           {R['Crushed Sapphire Ore']: 1, R['Water']: 000},
+           {R['Purified Sapphire Ore']: 1},
+           20, 6400, 2),
+    recipe(Machine.ORE_WASHER,
+           {R['Crushed Sapphire Ore']: 1, R['Distilled Water']: 100},
+           {R['Purified Sapphire Ore']: 1, R['Aluminium Dust']: 1/3, R['Stone Dust']: 1},
+           20, 6400),
+    recipe(Machine.THERMAL_CENTRIFUGE,
+           {R['Crushed Sapphire Ore']: 1},
+           {R['Refined Sapphire Ore']: 1, R['Green Sapphire Dust']: 1/3, R['Stone Dust']: 1},
+           20, 12000),
+    recipe(Machine.ORE_GRINDING,
+           {R['Crushed Sapphire Ore']: 1},
+           {R['Impure Pile of Sapphire Dust']: 1, R['Aluminium Dust']: chance(.14, .085)},
+           20, 800),
+    recipe(Machine.ORE_CRUSHING,
+           {R['Crushed Sapphire Ore']: 1},
+           {R['Impure Pile of Sapphire Dust']: 1},
+           .5, 160),
+    recipe(Machine.ORE_WASHER,
+           {R['Impure Pile of Sapphire Dust']: 1, R['Water']: 100},
+           {R['Sapphire Dust']: 1},
+           .4, 32, 2),
+    recipe(Machine.CENTRIFUGE,
+           {R['Impure Pile of Sapphire Dust']: 1},
+           {R['Sapphire Dust']: 1, R['Aluminium Dust']: 1},
+           4, 1920),
+    recipe(Machine.SIFTER,
+           {R['Purified Sapphire Ore']: 1},
+           {R['Exquisite Sapphire']: chance(.05, .015),
+            R['Flawless Sapphire']: chance(.15, .02),
+            R['Sapphire']: chance(.5, .1),
+            R['Purified Pile of Sapphire Dust']: chance(.25, .05)},
+           20, 6400),
+    recipe(Machine.THERMAL_CENTRIFUGE,
+           {R['Purified Sapphire Ore']: 1},
+           {R['Refined Sapphire Ore']: 1, R['Green Sapphire Dust']: 1/3},
+           20, 12000),
+    recipe(Machine.ORE_GRINDING,
+           {R['Purified Sapphire Ore']: 1},
+           {R['Purified Pile of Sapphire Dust']: 1, R['Green Sapphire Dust']: chance(.14, .085)},
+           20, 800),
+    recipe(Machine.ORE_CRUSHING,
+           {R['Purified Sapphire Ore']: 1},
+           {R['Purified Pile of Sapphire Dust']: 1},
+           .5, 160),
+    recipe(Machine.ORE_GRINDING,
+           {R['Refined Sapphire Ore']: 1},
+           {R['Sapphire Dust']: 1, R['Green Sapphire Dust']: chance(.14, .085)},
+           20, 800),
+    recipe(Machine.ORE_CRUSHING,
+           {R['Refined Sapphire Ore']: 1},
+           {R['Sapphire Dust']: 1},
+           .5, 160),
+    recipe(Machine.ORE_WASHER,
+           {R['Purified Pile of Sapphire Dust']: 1, R['Water']: 100},
+           {R['Sapphire Dust']: 1},
+           .4, 32, 2),
+    recipe(Machine.CENTRIFUGE,
+           {R['Purified Pile of Sapphire Dust']: 1},
+           {R['Sapphire Dust']: 1, R['Green Sapphire Dust']: 1/9},
+           5, 500),
+    recipe(Machine.ELECTROLYZER,
+           {R['Green Sapphire Dust']: 5},
+           {R['Aluminium Dust']: 2},
+           5, 3000),
+    recipe(Machine.ELECTROLYZER,
+           {R['Sapphire Dust']: 5},
+           {R['Aluminium Dust']: 2},
+           5, 3000),
+    recipe(Machine.CUTTER,
+           {R['Exquisite Sapphire']: 1, R['Lubricant']: 1},
+           {R['Flawless Sapphire']: 2},
+           1, 320),
+    recipe(Machine.CUTTER,
+           {R['Exquisite Sapphire']: 1, R['Water']: 4},
+           {R['Flawless Sapphire']: 2},
+           2, 640),
+    recipe(Machine.CUTTER,
+           {R['Exquisite Sapphire']: 1, R['Distilled Water']: 3},
+           {R['Flawless Sapphire']: 2},
+           1.5, 480),
+    recipe(Machine.LATHE,
+           {R['Exquisite Sapphire']: 1},
+           {R['Sapphire Lens']: 1, R['Sapphire Dust']: 2},
+           120, 72000),
+    recipe(Machine.PART_GRINDING,
+           {R['Exquisite Sapphire']: 1},
+           {R['Sapphire Dust']: 4},
+           4, 160),
+    recipe(Machine.PART_GRINDING,
+           {R['Sapphire Lens']: 1},
+           {R['Small Pile of Sapphire Dust']: 3},
+           .75, 30),
+    recipe(Machine.PACKER,
+           {R['Small Pile of Sapphire Dust']: 4},
+           {R['Sapphire Dust']: 1},
+           .5, 120, 1)
 )
 
 
@@ -189,14 +332,34 @@ def value_minus_cost(chain: Chain):
     return chain.value - chain.cost
 
 
-if __name__ == '__main__':
-    chains = map(chain, get_chains(R['Cobalt Dust']))
-    c = max(chains, key=lambda x: resource_efficiency(x, R['Cobalt Dust']))
+def value_over_cost(chain: Chain):
+    return chain.value / chain.cost
+
+
+def cost_over_value(chain: Chain):
+    return chain.cost / chain.value
+
+
+def cost_minus_value(chain: Chain):
+    return chain.cost - chain.value
+
+
+def print_chain(c: Chain):
     for i, r in enumerate(c.recipes, 1):
         print_box(r.to_lines(i), indent=2)
+    print(f'chain {c.id}')
+    print()
     for resource, quantity in sorted(c.flows.items(), key=lambda kv: kv[1]):
         print(f'* {round(quantity, 3):g} {resource}')
     print(f'value: {c.value:g}')
     print(f'cost: {c.cost:g}')
-    print(f'energy: {c.energy:g}')
-    print(f'duration: {c.duration:g}')
+    print(f'energy: {c.energy:g}EU')
+    print(f'duration: {c.duration:g}s')
+
+
+if __name__ == '__main__':
+    target = R['Aluminium Dust']
+    chains = map(chain, get_chains(target))
+    
+    c = max(chains, key=lambda x: resource_efficiency(x, target))
+    print_chain(c)
